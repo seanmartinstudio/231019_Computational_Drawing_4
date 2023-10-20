de56164c628eeaa2251a0dc84b59e826a2afc631
Sean Martin <sean@seanmartinstudio.com>
2023-10-20 10:51:12 -0700

    refactors code

--- sketch.js
+++ sketch.js
@@ -1,39 +1,43 @@
 let timer
-
-let inc = 0.01;
-let start = 0;
+let inc = 0.01
+let start = 0
 let windowSizeX = 600
 let windowSizeY = 600
 let circleSize = 20
 
-let backgroundColor = 0
-
-
-
 function setup() {
- 
-
-  if(windowWidth < 700) {
+  //Using JS for mobile devise media query
+  if(windowWidth < 768) {
     windowSizeX = 350
     windowSizeY = 350
     circleSize = 10
   }
-
   createCanvas(windowSizeX, windowSizeY);
-  
-
-  background(backgroundColor);
-  timer = millis();
-
-    // Create a button
-    let button = createButton('Create New Drawing');
-    button.position(10, 10);
-  
-    // Add an event listener to the button
-    button.mousePressed(buttonClicked);
+  background(0)
+  timer = millis()
+  buildButton()
 }
 
 function draw() {
+  buildShape()
+  start += inc;
+  saveSketch()
+}
+
+//Helper functions below
+
+function buildButton() {
+  let button = createButton('Create New Drawing')
+  button.position(10, 10)
+  button.mousePressed(buttonClicked)
+}
+
+function buttonClicked() {
+  clear()
+  loop()
+}
+
+function buildShape() {
   beginShape(POINTS);
   let xoff = start;
   let yoff = start + 10
@@ -44,38 +48,22 @@ function draw() {
   vertex(x, y);
   xoff += inc;
   endShape();
-  
-  start += inc;
-  
-  saveSketch()
 }
 
 function saveSketch() {
-      if (millis() - timer >= 15000) {
+  if (millis() - timer >= 15000) {
     // Restart the sketch by resetting the timer
     timer = millis();
-   
     noLoop()
     const today = new Date();
-
-    // Extract the year, month, and day components
     const year = today.getFullYear();
     const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based
     const day = today.getDate().toString().padStart(2, '0');
-
-    // Generate a filename with the sketch name, date, and '.jpg' extension
     const filename = `${year}_${month}_${day}_SketchName`;
 
     // Save the canvas as a jpg with the generated filename
     // saveCanvas(filename, 'jpg');
   }
-}
-
-
-
-function buttonClicked() {
-  clear()
-  loop()
 }
 
 
